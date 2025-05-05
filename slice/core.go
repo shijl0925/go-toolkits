92fb917354be8a9237fe234bfeb055a5c31d32a1
Edmond Dantès <18221510672@139.com>
2025-05-05 19:09:25 +0800

    add Max Min

--- slice/core.go
+++ slice/core.go
@@ -2,14 +2,38 @@ package slice
 
 import "golang.org/x/exp/constraints"
 
-type Number interface {
+type RealNumber interface {
 	constraints.Integer | constraints.Float
 }
 
-func Sum[T Number](s []T) T {
+type Number interface {
+	RealNumber | ~complex64 | ~complex128
+}
+
+func Sum[T RealNumber](s []T) T {
 	var result T
 	for _, v := range s {
 		result += v
+	}
+	return result
+}
+
+func Max[T constraints.Ordered](s []T) T {
+	result := s[0]
+	for _, v := range s[1:] {
+		if v > result {
+			result = v
+		}
+	}
+	return result
+}
+
+func Min[T constraints.Ordered](s []T) T {
+	result := s[0]
+	for _, v := range s[1:] {
+		if v < result {
+			result = v
+		}
 	}
 	return result
 }
--- slice/core_test.go
+++ slice/core_test.go
@@ -5,15 +5,51 @@ import (
 	"testing"
 )
 
-func Test_MapSliceArray(t *testing.T) {
+func Test_SumSlice(t *testing.T) {
 	t.Run("test1", func(t *testing.T) {
 		if got := slice.Sum([]int{1, 2, 3, 4, 5}); got != 15 {
-			t.Errorf("Reverse() expected %v, got %v", 15, got)
+			t.Errorf("Sum() expected %v, got %v", 15, got)
 		}
 	})
 	t.Run("test2", func(t *testing.T) {
 		if got := slice.Sum([]float64{1.1, 2.0, 3.5}); got != 6.6 {
-			t.Errorf("Reverse() expected %v, got %v", 15, got)
+			t.Errorf("Sum() expected %v, got %v", 6.6, got)
+		}
+	})
+}
+
+func Test_MaxSlice(t *testing.T) {
+	t.Run("test1", func(t *testing.T) {
+		if got := slice.Max([]int{-1, -5, -3, -4, -2}); got != -1 {
+			t.Errorf("Max() expected %v, got %v", -1, got)
+		}
+	})
+	t.Run("test2", func(t *testing.T) {
+		if got := slice.Max([]float64{1.1, 3.5, 2.0, 0.1}); got != 3.5 {
+			t.Errorf("Max() expected %v, got %v", 3.5, got)
+		}
+	})
+	t.Run("test3", func(t *testing.T) {
+		if got := slice.Max([]int{-1, 5, 3, 4, 2}); got != 5 {
+			t.Errorf("Max() expected %v, got %v", 5, got)
+		}
+	})
+}
+
+func Test_MinSlice(t *testing.T) {
+	t.Run("test1", func(t *testing.T) {
+		if got := slice.Min([]int{-1, 5, 3, 4, 2}); got != -1 {
+			t.Errorf("Min() expected %v, got %v", -1, got)
+		}
+	})
+	t.Run("test2", func(t *testing.T) {
+		if got := slice.Min([]float64{1.1, 3.5, 2.0, 0.1}); got != 0.1 {
+			t.Errorf("Min() expected %v, got %v", 0.1, got)
+		}
+	})
+	t.Run("test3", func(t *testing.T) {
+		if got := slice.Min([]int{-1, -5, -3, -4, -2}); got != -5 {
+			t.Errorf("Min() expected %v, got %v", -5, got)
 		}
 	})
 }
