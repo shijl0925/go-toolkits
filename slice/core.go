527657aff9d008e66b422761eec67a0feef26bfc
Edmond Dantès <18221510672@139.com>
2025-05-05 20:58:10 +0800

    slice functons

--- slice/core.go
+++ slice/core.go
@@ -41,7 +41,51 @@ func Min[T constraints.Ordered](s []T) T {
 	return result
 }
 
+// Pop removes and returns the last element from a slice.
+// 删除并返回最后一个元素。
+func Pop[T any](s []T) (T, []T) {
+	if len(s) == 0 {
+		var zero T
+		return zero, s
+	}
+	return s[len(s)-1], s[:len(s)-1]
+}
+
+// Delete removes and returns the element at a specific index from a slice.
+// 删除位于索引 index 的元素。
+func Delete[T any](s []T, index int) []T {
+	if index >= len(s) {
+		return s
+	}
+	return append(s[:index], s[index+1:]...)
+}
+
+// DeleteMany removes and returns the elements between start and end from a slice.
+// 删除位于索引 start 和 end 之间的元素。
+func DeleteMany[T any](s []T, start, end int) []T {
+	return append(s[:start], s[end:]...)
+}
+
+// Append appends one or more elements to a slice.
+// 追加一个或多个元素到 slice。
+func Append[T any](src []T, elements ...T) []T {
+	return append(src, elements...)
+}
+
+// Extend extends a slice by appending one or more elements.
+// 扩展 slice，追加一个新切片。
+func Extend[T any](src []T, elements []T) []T {
+	return append(src, elements...)
+}
+
 // Insert inserts an element at a specific index in a slice.
+// 在 slice 的指定位置插入一个元素。
 func Insert[T any](src []T, element T, index int) []T {
 	return append(src[:index], append([]T{element}, src[index:]...)...)
+}
+
+// InsertMany inserts multiple elements at a specific index in a slice.
+// 在 slice 的指定位置插入新切片。
+func InsertMany[T any](src []T, elements []T, index int) []T {
+	return append(src[:index], append(elements, src[index:]...)...)
 }
--- slice/core_test.go
+++ slice/core_test.go
@@ -7,52 +7,59 @@ import (
 )
 
 func Test_SumSlice(t *testing.T) {
-	t.Run("test1", func(t *testing.T) {
-		if got := slice.Sum([]int{1, 2, 3, 4, 5}); got != 15 {
-			t.Errorf("Sum() expected %v, got %v", 15, got)
-		}
-	})
-	t.Run("test2", func(t *testing.T) {
-		if got := slice.Sum([]float64{1.1, 2.0, 3.5}); got != 6.6 {
-			t.Errorf("Sum() expected %v, got %v", 6.6, got)
-		}
-	})
+	var tests = []struct {
+		name     string
+		slice    []float64
+		expected float64
+	}{
+		{"test1", []float64{1, 2, 3, 4, 5}, 15},
+		{"test2", []float64{1.1, 2.0, 3.5}, 6.6},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := slice.Sum(tc.slice); got != tc.expected {
+				t.Errorf("Sum() expected %v, got %v", tc.expected, got)
+			}
+		})
+	}
 }
 
 func Test_MaxSlice(t *testing.T) {
-	t.Run("test1", func(t *testing.T) {
-		if got := slice.Max([]int{-1, -5, -3, -4, -2}); got != -1 {
-			t.Errorf("Max() expected %v, got %v", -1, got)
-		}
-	})
-	t.Run("test2", func(t *testing.T) {
-		if got := slice.Max([]float64{1.1, 3.5, 2.0, 0.1}); got != 3.5 {
-			t.Errorf("Max() expected %v, got %v", 3.5, got)
-		}
-	})
-	t.Run("test3", func(t *testing.T) {
-		if got := slice.Max([]int{-1, 5, 3, 4, 2}); got != 5 {
-			t.Errorf("Max() expected %v, got %v", 5, got)
-		}
-	})
+	var tests = []struct {
+		name     string
+		slice    []float64
+		expected float64
+	}{
+		{"test1", []float64{-1, -5, -3, -4, -2}, -1},
+		{"test2", []float64{1.1, 3.5, 2.0, 0.1}, 3.5},
+		{"test3", []float64{-1, 5, 3, 4, 2}, 5},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := slice.Max(tc.slice); got != tc.expected {
+				t.Errorf("Max() expected %v, got %v", tc.expected, got)
+			}
+		})
+	}
 }
 
 func Test_MinSlice(t *testing.T) {
-	t.Run("test1", func(t *testing.T) {
-		if got := slice.Min([]int{-1, 5, 3, 4, 2}); got != -1 {
-			t.Errorf("Min() expected %v, got %v", -1, got)
-		}
-	})
-	t.Run("test2", func(t *testing.T) {
-		if got := slice.Min([]float64{1.1, 3.5, 2.0, 0.1}); got != 0.1 {
-			t.Errorf("Min() expected %v, got %v", 0.1, got)
-		}
-	})
-	t.Run("test3", func(t *testing.T) {
-		if got := slice.Min([]int{-1, -5, -3, -4, -2}); got != -5 {
-			t.Errorf("Min() expected %v, got %v", -5, got)
-		}
-	})
+	var tests = []struct {
+		name     string
+		slice    []float64
+		expected float64
+	}{
+		{"test1", []float64{-1, 5, 3, 4, 2}, -1},
+		{"test2", []float64{1.1, 3.5, 2.0, 0.1}, 0.1},
+		{"test3", []float64{-1, -5, -3, -4, -2}, -5},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := slice.Min(tc.slice); got != tc.expected {
+				t.Errorf("Min() expected %v, got %v", tc.expected, got)
+			}
+		})
+	}
 }
 
 func Test_Insert(t *testing.T) {
@@ -71,6 +78,151 @@ func Test_Insert(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			if got := slice.Insert(tc.slice, tc.element, tc.index); !reflect.DeepEqual(got, tc.expected) {
 				t.Errorf("Map() expected %v, got %v", tc.expected, got)
+			}
+		})
+	}
+}
+
+func Test_InsertMany(t *testing.T) {
+	var tests = []struct {
+		name     string
+		slice    []int
+		elements []int
+		index    int
+		expected []int
+	}{
+		{"test1", []int{}, []int{1}, 0, []int{1}},
+		{"test2", []int{1, 2, 3, 4}, []int{2, 3}, 1, []int{1, 2, 3, 2, 3, 4}},
+		{"test3", []int{1, 2, 3, 4, 5, 6}, []int{7}, 6, []int{1, 2, 3, 4, 5, 6, 7}},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := slice.InsertMany(tc.slice, tc.elements, tc.index); !reflect.DeepEqual(got, tc.expected) {
+				t.Errorf("Map() expected %v, got %v", tc.expected, got)
+			}
+		})
+	}
+}
+
+func Test_Pop(t *testing.T) {
+	var s, want []string
+	var lastWant string
+	t.Run("test0", func(t *testing.T) {
+		if last, got := slice.Pop(s); !reflect.DeepEqual(got, want) {
+			t.Errorf("Pop() expected %v, got %v", want, got)
+			if last != lastWant {
+				t.Errorf("Pop() element expected %v, got %v", lastWant, last)
+			}
+		}
+
+	})
+	var tests = []struct {
+		name     string
+		slice    []int
+		expected []int
+	}{
+		{"test1", []int{1, 2, 3, 4}, []int{1, 2, 3}},
+		{"test2", []int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4}},
+		{"test3", []int{1, 2, 3, 4, 5, 6}, []int{1, 2, 3, 4, 5}},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if last, got := slice.Pop(tc.slice); !reflect.DeepEqual(got, tc.expected) {
+				t.Errorf("Pop() expected %v, got %v", tc.expected, got)
+				if last != tc.slice[len(tc.slice)-1] {
+					t.Errorf("Pop() element expected %v, got %v", last, tc.slice[len(tc.slice)-1])
+				}
+			}
+		})
+	}
+}
+
+func Test_Delete(t *testing.T) {
+	var tests = []struct {
+		name     string
+		slice    []int
+		index    int
+		expected []int
+	}{
+		{"test1", []int{1, 2, 3, 4}, 1, []int{1, 3, 4}},
+		{"test2", []int{1, 2, 3, 4}, 3, []int{1, 2, 3}},
+		{"test3", []int{1, 2, 3, 4}, 4, []int{1, 2, 3, 4}},
+		{"test4", []int{1, 2, 3, 4, 5, 6}, 2, []int{1, 2, 4, 5, 6}},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := slice.Delete(tc.slice, tc.index); !reflect.DeepEqual(got, tc.expected) {
+				t.Errorf("Delete() expected %v, got %v", tc.expected, got)
+			}
+		})
+	}
+}
+
+func Test_DeleteMany(t *testing.T) {
+	var tests = []struct {
+		name     string
+		slice    []int
+		start    int
+		end      int
+		expected []int
+	}{
+		{"test1", []int{1, 2, 3, 4}, 1, 2, []int{1, 3, 4}},
+		{"test2", []int{1, 2, 3, 4, 5}, 2, 4, []int{1, 2, 5}},
+		{"test3", []int{1, 2, 3, 4, 5, 6}, 4, 5, []int{1, 2, 3, 4, 6}},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := slice.DeleteMany(tc.slice, tc.start, tc.end); !reflect.DeepEqual(got, tc.expected) {
+				t.Errorf("DeleteMany() expected %v, got %v", tc.expected, got)
+			}
+		})
+	}
+}
+
+func Test_Append(t *testing.T) {
+	var tests = []struct {
+		name     string
+		slice    []int
+		element  int
+		expected []int
+	}{
+		{"test1", []int{}, 1, []int{1}},
+		{"test2", []int{1, 2, 3, 4}, 5, []int{1, 2, 3, 4, 5}},
+		{"test3", []int{1, 2, 3, 4, 5, 6}, 7, []int{1, 2, 3, 4, 5, 6, 7}},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := slice.Append(tc.slice, tc.element); !reflect.DeepEqual(got, tc.expected) {
+				t.Errorf("Append() expected %v, got %v", tc.expected, got)
+			}
+		})
+	}
+
+	slice1 := []int{1, 2, 3, 4}
+	elements := []int{5, 6, 7}
+	want := []int{1, 2, 3, 4, 5, 6, 7}
+	t.Run("test0", func(t *testing.T) {
+		if got := slice.Append(slice1, elements...); !reflect.DeepEqual(got, want) {
+			t.Errorf("Append() expected %v, got %v", want, got)
+		}
+	})
+}
+
+func Test_Extend(t *testing.T) {
+	var tests = []struct {
+		name     string
+		slice    []int
+		elements []int
+		expected []int
+	}{
+		{"test1", []int{}, []int{5, 6, 7}, []int{5, 6, 7}},
+		{"test2", []int{1, 2, 3, 4}, []int{5, 6, 7}, []int{1, 2, 3, 4, 5, 6, 7}},
+		{"test3", []int{1, 2, 3, 4, 5, 6}, []int{7}, []int{1, 2, 3, 4, 5, 6, 7}},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := slice.Extend(tc.slice, tc.elements); !reflect.DeepEqual(got, tc.expected) {
+				t.Errorf("Extend() expected %v, got %v", tc.expected, got)
 			}
 		})
 	}
