8cb4868c76d35b095844955e626a3b3b94b54ca7
Edmond Dantès <18221510672@139.com>
2025-05-10 16:38:45 +0800

    json encode decode

---
+++ jsonutil/core.go
@@ -0,0 +1,107 @@
+package jsonutil
+
+import (
+	"encoding/json"
+	"io"
+	"strings"
+)
+
+// Encode encode data to json bytes. alias of json.Marshal
+func Encode(v any) ([]byte, error) {
+	return json.Marshal(v)
+}
+
+// Dumps encode data to json string, panic if error
+// 将数据编码为JSON文本，如果发生错误则panic
+//
+// Example:
+//
+// testUser = Person{"Alice", "Smith", 18}
+// res, _ := jsonutil.Dumps(testUser) // res == `{"first_name":"Alice","last_name":"Smith","age":18}`
+func Dumps(v any) string {
+	b, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
+}
+
+// DumpsPretty encode data to json string with pretty format
+// 将数据编码为带格式的JSON文本, 如果发生错误则panic
+//
+// Example:
+//
+// testUser = Person{"Alice", "Smith", 18}
+// res, _ := jsonutil.DumpsPretty(testUser, 4)
+//
+//	res == `{
+//		   "first_name": "Alice",
+//		   "last_name": "Smith",
+//		   "age": 18
+//		}`
+func DumpsPretty(v any, indent int) string {
+	if indent <= 0 {
+		return Dumps(v)
+	}
+	indentStr := strings.Repeat(" ", indent)
+	b, err := json.MarshalIndent(v, "", indentStr)
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
+}
+
+// EncodeToWriter encode data to json and write to writer.
+// 将数据编码为JSON并写入writer
+//
+// Example:
+//
+// testUser = Person{"Alice", "Smith", 18}
+// file, _ := os.OpenFile("test.json", os.O_CREATE|os.O_WRONLY, 0666)
+// defer file.Close()
+// err := jsonutil.EncodeToWriter(testUser, file)
+// if err != nil {...}
+func EncodeToWriter(v any, w io.Writer) error {
+	return json.NewEncoder(w).Encode(v)
+}
+
+// Decode decode json bytes to data ptr. alias of json.Unmarshal
+//
+// Example:
+//
+// var testUser Person
+// data := []byte(`{"first_name":"Alice","last_name":"Smith","age":18}`)
+// err := jsonutil.Decode(data, &testUser)
+// if err != nil {...}
+// fmt.Println(testUser) // {18 Alice Smith}
+func Decode(data []byte, ptr any) error {
+	return json.Unmarshal(data, ptr)
+}
+
+// Loads decode json string to data ptr, panic if error
+// 将JSON文本解码为数据指针，如果发生错误则恐慌
+//
+// Example:
+//
+// var testUser Person
+// str := `{"first_name":"Alice","last_name":"Smith","age":18}`
+// err := jsonutil.Loads(str, &testUser)
+// if err != nil {...}
+// fmt.Println(testUser) // {18 Alice Smith}
+func Loads(s string, ptr any) error {
+	return json.Unmarshal([]byte(s), ptr)
+}
+
+// DecodeFromReader decode JSON from io reader.
+// 从io.Reader中解码JSON文本到数据指针
+//
+// Example:
+//
+// var testUser Person
+// file, _ := os.Open("test.json")
+// err := jsonutil.DecodeFromReader(file, &testUser)
+// if err != nil {...}
+// fmt.Println(testUser) // {18 Alice Smith}
+func DecodeFromReader(r io.Reader, ptr any) error {
+	return json.NewDecoder(r).Decode(ptr)
+}
---
+++ jsonutil/core_test.go
@@ -0,0 +1,168 @@
+package jsonutil_test
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/shijl0925/go-toolkits/jsonutil"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+type Person struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Age       int    `json:"age"`
+}
+
+var testUser = Person{"Alice", "Smith", 18}
+
+func Test_Encode(t *testing.T) {
+	want := `{"first_name":"Alice","last_name":"Smith","age":18}`
+	t.Run("test1", func(t *testing.T) {
+		got, err := jsonutil.Encode(testUser)
+		if err != nil {
+			t.Errorf("Encode() error = %v", err)
+		}
+		if string(got) != want {
+			t.Errorf("Encode() expected %v, got %v", want, got)
+		}
+	})
+}
+
+func Test_Dumps(t *testing.T) {
+	want := `{"first_name":"Alice","last_name":"Smith","age":18}`
+	t.Run("test1", func(t *testing.T) {
+		if got := jsonutil.Dumps(testUser); got != want {
+			t.Errorf("Dumps() expected %v, got %v", want, got)
+		}
+	})
+}
+
+func Test_DumpsPretty(t *testing.T) {
+	want := `{
+    "first_name": "Alice",
+    "last_name": "Smith",
+    "age": 18
+}`
+	t.Run("test1", func(t *testing.T) {
+		if got := jsonutil.DumpsPretty(testUser, 4); got != want {
+			t.Errorf("DumpsPretty() expected %v, got %v", want, got)
+		}
+	})
+}
+
+func Test_EncodeToWriter(t *testing.T) {
+	t.Run("test1", func(t *testing.T) {
+		buf := &bytes.Buffer{}
+		want := `{"first_name":"Alice","last_name":"Smith","age":18}
+`
+		err := jsonutil.EncodeToWriter(testUser, buf)
+		if err != nil {
+			t.Errorf("EncodeToWriter() error: %v", err)
+		}
+		if buf.String() != want {
+			t.Errorf("EncodeToWriter() expected %v, got %v", want, buf.String())
+		}
+	})
+
+	t.Run("test2", func(t *testing.T) {
+		tmpfile, _ := ioutil.TempFile("", "example.json")
+		err := jsonutil.EncodeToWriter(testUser, tmpfile)
+		want := `{"first_name":"Alice","last_name":"Smith","age":18}
+`
+
+		if err != nil {
+			t.Errorf("EncodeToWriter() error: %v", err)
+		}
+
+		buf, err := ioutil.ReadFile(tmpfile.Name())
+		if string(buf) != want {
+			t.Errorf("EncodeToWriter() expected %v, got %v", want, string(buf))
+		}
+	})
+}
+
+func Test_Decode(t *testing.T) {
+	var user = &Person{}
+	str := `{"first_name":"Alice","last_name":"Smith","age":18}`
+	t.Run("test1", func(t *testing.T) {
+		err := jsonutil.Decode([]byte(str), user)
+		if err != nil {
+			t.Errorf("Decode() error = %v", err)
+		}
+		if user.FirstName != "Alice" {
+			t.Errorf("Decode() expected %v, got %v", "Alice", user.FirstName)
+		}
+		if user.LastName != "Smith" {
+			t.Errorf("Decode() expected %v, got %v", "Smith", user.LastName)
+		}
+		if user.Age != 18 {
+			t.Errorf("Decode() expected %v, got %v", 18, user.Age)
+		}
+	})
+}
+
+func Test_Loads(t *testing.T) {
+	var user = &Person{}
+	str := `{"first_name":"Alice","last_name":"Smith","age":18}`
+	t.Run("test1", func(t *testing.T) {
+		err := jsonutil.Loads(str, user)
+		if err != nil {
+			t.Errorf("Loads() error = %v", err)
+		}
+		if user.FirstName != "Alice" {
+			t.Errorf("Loads() expected %v, got %v", "Alice", user.FirstName)
+		}
+		if user.LastName != "Smith" {
+			t.Errorf("Loads() expected %v, got %v", "Smith", user.LastName)
+		}
+		if user.Age != 18 {
+			t.Errorf("Loads() expected %v, got %v", 18, user.Age)
+		}
+	})
+}
+
+func Test_DecodeFromReader(t *testing.T) {
+	t.Run("test1", func(t *testing.T) {
+		var user1 = &Person{}
+
+		want := `{"first_name":"Alice","last_name":"Smith","age":18}`
+
+		buf := &bytes.Buffer{}
+		buf.WriteString(want)
+		fmt.Printf("buf: %s\n", buf.String())
+
+		err := jsonutil.DecodeFromReader(buf, user1)
+		if err != nil {
+			t.Errorf("DecodeFromReader() error: %v", err)
+		}
+		if user1.FirstName != "Alice" {
+			t.Errorf("DecodeFromReader() expected %v, got %v", "Alice", user1.FirstName)
+		}
+		if user1.LastName != "Smith" {
+			t.Errorf("DecodeFromReader() expected %v, got %v", "Smith", user1.LastName)
+		}
+		if user1.Age != 18 {
+			t.Errorf("DecodeFromReader() expected %v, got %v", 18, user1.Age)
+		}
+	})
+
+	t.Run("test2", func(t *testing.T) {
+		var user2 = &Person{}
+		file, _ := os.Open("testdata/test.json")
+		err := jsonutil.DecodeFromReader(file, user2)
+		if err != nil {
+			t.Errorf("DecodeFromReader() error: %v", err)
+		}
+		if user2.FirstName != "Alice" {
+			t.Errorf("DecodeFromReader() expected %v, got %v", "Alice", user2.FirstName)
+		}
+		if user2.LastName != "Smith" {
+			t.Errorf("DecodeFromReader() expected %v, got %v", "Smith", user2.LastName)
+		}
+		if user2.Age != 18 {
+			t.Errorf("DecodeFromReader() expected %v, got %v", 18, user2.Age)
+		}
+	})
+}
