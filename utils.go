6e6f419dcfc07474863430e73fd7b31e7f708d43
Edmond Dantès <18221510672@139.com>
2025-05-14 19:56:31 +0800

    Update utils.go

--- utils.go
+++ utils.go
@@ -1,7 +1,11 @@
 package toolkits
 
 import (
+	"bytes"
+	"encoding/binary"
+	"encoding/json"
 	"fmt"
+	"math"
 	"reflect"
 	"strconv"
 	"time"
@@ -244,6 +248,82 @@ func SafeToFloat64(v any) (float64, error) {
 	}
 }
 
+
+func SafeToBytes(v any) ([]byte, error) {
+	if v == nil {
+		return nil, ErrNilValue
+	}
+
+	val := reflect.ValueOf(v)
+
+	if val.Kind() == reflect.Ptr {
+		if val.IsNil() {
+			return nil, ErrNilPointer
+		}
+		val = val.Elem()
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		return []byte(val.String()), nil
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		number := val.Int()
+		buf := bytes.NewBuffer(nil)
+		err := binary.Write(buf, binary.BigEndian, number)
+		return buf.Bytes(), err
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		number := val.Uint()
+		buf := bytes.NewBuffer(nil)
+		err := binary.Write(buf, binary.BigEndian, number)
+		return buf.Bytes(), err
+	case reflect.Float32:
+		number := float32(val.Float())
+		bits := math.Float32bits(number)
+		bs := make([]byte, 4)
+		binary.BigEndian.PutUint32(bs, bits)
+		return bs, nil
+	case reflect.Float64:
+		number := val.Float()
+		bits := math.Float64bits(number)
+		bs := make([]byte, 8)
+		binary.BigEndian.PutUint64(bs, bits)
+		return bs, nil
+	case reflect.Bool:
+		return strconv.AppendBool([]byte{}, val.Bool()), nil
+	default:
+		return json.Marshal(v)
+	}
+}
+
+// SafeToInterface converts reflect value to its interface type.
+func SafeToInterface(v reflect.Value) (interface{}, bool) {
+	if !v.IsValid() {
+		return nil, false
+	}
+
+	if v.CanInterface() {
+		return v.Interface(), true
+	}
+
+	switch v.Kind() {
+	case reflect.Bool:
+		return v.Bool(), true
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int(), true
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return v.Uint(), true
+	case reflect.Float32, reflect.Float64:
+		return v.Float(), true
+	case reflect.Complex64, reflect.Complex128:
+		return v.Complex(), true
+	case reflect.String:
+		return v.String(), true
+	case reflect.Ptr, reflect.Interface:
+		return SafeToInterface(v.Elem())
+	default:
+		return nil, false
+	}
+}
 // IsKindOf checks if the given value is an instance of the given kind.
 // It returns true if the value is an instance of the given kind, false otherwise.
 func IsKindOf(s any, t reflect.Kind) bool {
