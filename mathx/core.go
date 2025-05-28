e3b04047ac30fc9eb06c6883e7c2442157972ad2
Edmond Dantès <18221510672@139.com>
2025-05-28 19:16:16 +0800

    Create mathx

---
+++ mathx/core.go
@@ -0,0 +1,38 @@
+package mathx
+
+import (
+	"fmt"
+	toolkits "github.com/shijl0925/go-toolkits"
+	"math"
+)
+
+// RoundToFloat returns the float32/float64 of the specified precision from rounding
+func RoundToFloat[T float64 | float32](f T, n int) float64 {
+	pow10N := math.Pow(10.0, float64(n))
+	val := float64(f)
+	return math.Round(val*pow10N) / pow10N
+}
+
+// FloatToPercent returns the percent string of the specified precision
+func FloatToPercent[T float64 | float32](f T, n uint) string {
+	return fmt.Sprintf("%.*f%%", n, RoundToFloat(f*100.0, int(n)))
+}
+
+// PercentToFloat returns the float64 of the percent string
+func PercentToFloat(s string) (float64, error) {
+	if len(s) < 1 {
+		return 0, fmt.Errorf("invalid percent string: %s", s)
+	}
+
+	if s[len(s)-1] != '%' {
+		return 0, fmt.Errorf("invalid percent string: missing '%%' suffix in '%s'", s)
+	}
+
+	s = s[:len(s)-1]
+	f, err := toolkits.AnyToFloat64(s)
+	if err != nil {
+		return 0, fmt.Errorf("failed to convert percent value to float64: %v", err)
+	}
+
+	return f / 100.0, nil
+}
